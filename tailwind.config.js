5cbf0df52870e3c69be87c01855c819a1c739e5b
Matheus Zanoni <matheuzzanoni@gmail.com>
2024-04-09 14:14:40 -0300

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+
     extend: {
       backgroundImage: {
         'filmes': "url('.././img/backgroundnovo.jpg')"
